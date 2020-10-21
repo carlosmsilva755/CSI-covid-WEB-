82d1e07e522219d9f931b347a426adfb5db3874f
carlosmsilva755 <carlosmsilva755@gmail.com>
2020-10-21 01:03:02 -0300

    feat: admin backup

--- src/pages/_BackUp/index.js
+++ src/pages/_BackUp/index.js
@@ -8,33 +8,62 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import './styles.css'
 import Header from '../../components/Header/Admin/index'
 import { AuthUserContext, withAuthorization } from '../../contexts/Session'
+import api from '../../services/api'
 
 const BackUp = (props) => {
 
     const history = useHistory()
     const width = window.innerWidth
+
+    const [token, setToken] = useState('')
     
     const [isAuth, setIsAuth] = useState(' ')
     const [showModal, setShowModal] = useState(false)
+    const [showBackupModal, setShowBackupModal] = useState(false)
+    const [showErrorModal, setShowErrorModal] = useState(false)
+
 
     useEffect(() => {
 
         props.firebase.auth.currentUser.getIdTokenResult()
         .then((idTokenResult) => {
-           if (!!idTokenResult.claims.admin) {
-             setIsAuth(true)
-           } else {
-             setIsAuth(false)
-           }
+            if (!!idTokenResult.claims.admin) {
+                setIsAuth(true)
+                setToken(idTokenResult.token)
+            } else {
+                setIsAuth(false)
+            }
         })
         .catch((error) => {
-          console.log(error);
+            console.log(error);
         })
 
     })
 
     const handleCloseModal = () => {
         setShowModal(false)
+    }
+
+    const handleCloseBackupModal= () => {
+        setShowBackupModal(false)
+    }
+
+    const handleBackup = () => {
+
+        const config = {
+            headers: { authorization: `Bearer ${token}` }
+        }
+
+        api.get('/backup', config)
+        .then(response=>{
+            setShowBackupModal(true)
+            setShowModal(false)
+        }).catch(error=>{
+            setShowErrorModal(true)
+            setShowBackupModal(true)
+            setShowModal(false)
+        })
+
     }
 
     return (
@@ -68,7 +97,21 @@ const BackUp = (props) => {
                             </DialogTitle>
                             <DialogActions>
                                 <button id='cancelar-button-backup'onClick={handleCloseModal} className='button-back'>Cancelar</button>
-                                <button id='confirmar-button'onClick={handleCloseModal} className='button button-modal'>Confirmar</button>
+                                <button id='confirmar-button'onClick={handleBackup} className='button button-modal'>Confirmar</button>
+                            </DialogActions>
+                        </Dialog>
+
+                        <Dialog
+                            open={showBackupModal} 
+                            aria-labelledby="draggable-dialog-title" maxWidth='xs'
+                        >
+                            <DialogTitle >
+                                <p className='modal-backup-text'>
+                                    {showErrorModal? 'Erro ao realizar backup!':'Backup realizado com sucesso!'}
+                                </p> 
+                            </DialogTitle>
+                            <DialogActions>
+                                <button id='cancelar-button-backup'onClick={handleCloseBackupModal} className='button-back'>Fechar</button>
                             </DialogActions>
                         </Dialog>
 
