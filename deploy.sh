1d8aee95e3e63bbbc933f82874d46de9908a8edf
Arilton Junior de Aguilar Nunes <arilton.j.aguilar@gmail.com>
2020-11-20 15:29:34 +0000

    Add new file

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+PRIVATE_KEY=$PRIVATE_KEY
+eval $(ssh-agent -s)
+echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+
+./disableHostKeyChecking.sh
+
+
+DEPLOY_SERVERS=$DEPLOY_SERVERS
+
+echo "deploying to $DEPLOY_SERVERS"
+ssh ubuntu@$DEPLOY_SERVERS 'bash' < ./updateAndRestart.sh
+scp -r build ubuntu@$DEPLOY_SERVERS:/home/ubuntu/apps/build/
+ssh ubuntu@$DEPLOY_SERVERS 'bash' < ./verifyFiles.sh
