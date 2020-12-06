0c1c4e15a81e6c540b118181a83c905dbd8c61c9
TerraLab <forum.terralab@gmail.com>
2020-12-06 20:36:19 +0000

    Update verifyFiles.sh

--- verifyFiles.sh
+++ verifyFiles.sh
@@ -6,6 +6,6 @@ cd /var/www/
 if [ -d "csicovidweb.tk" ]; then sudo rm -Rf csicovidweb.tk; fi
 sudo mkdir csicovidweb.tk
 cd csicovidweb.tk
-sudo mv /home/ubuntu/apps/build* .
+sudo mv /home/ubuntu/apps/build/* .
 sudo chown -R www-data:www-data /var/www/csicovidweb.tk
 sudo systemctl restart nginx
